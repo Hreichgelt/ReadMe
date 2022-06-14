21af0eaf59843be733555f8eb5355420c3e5b645
Hans Reichgelt <hreichgelt06@gmail.com>
2022-06-14 09:48:57 -0400

    starting over multiple times, trying to work through the process, tried with export and require with multiple file locations, back to one page

--- index.js
+++ index.js
@@ -1,33 +1,82 @@
-// GIVEN a command-line application that accepts user input
-// WHEN I am prompted for information about my application repository
-// THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
-// WHEN I enter my project title
-// THEN this is displayed as the title of the README
-// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
-// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
-// WHEN I choose a license for my application from a list of options
-// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
-// WHEN I enter my GitHub username
-// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
-// WHEN I enter my email address
-// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
-// WHEN I click on the links in the Table of Contents
-// THEN I am taken to the corresponding section of the README
-
-
-
-
-
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// page where readme gets created to 
+const createReadme = require('./Utilities/generateMarkdown.js');
+// const questions = require('./questions.js');
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// array of questions for user
+const questions = () => {
+    return inquirer. prompt([
+        {
+            type: 'input',
+            name: 'Project',
+            message: 'What is your projects name?',
+        },
+        {
+            type: 'input',
+            name: 'Live Site',
+            message: 'please include your live site here',
+        },
+        {
+            type: 'checkbox',
+            message: 'What items would you like included in the table of contents?',
+            name: 'stack',
+            choices: ['Description', 'Usage', 'Visuals', 'Acknowledgments', 'Resources'],
+        },
+        {
+            type: 'checkbox',
+            message: 'What languages are in the project?',
+            name: 'stack',
+            choices: ['HTML', 'CSS', 'JavaScript', 'MySQL', 'NodeJS'],
+        },
+        {
+            type: 'input',
+            name: 'Description',
+            message: 'please include your description of the site here',
+        },
+        {
+            type: 'input',
+            name: 'Usage',
+            message: 'please include your explaination on how to use the site here',
+        },
+        {
+            type: 'input',
+            name: 'Authors and Acknowledgments',
+            message: 'please include authors and contributors here',
+        },
+        {
+            type: 'input',
+            name: 'Resources',
+            message: 'please include your live site and repository links here',
+        },
+        {
+            type: 'input',
+            name: 'Visuals',
+            message: 'please include your demo or visuals here',
+        },
+    ])
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
+const writeFile = data => {
+    fs.writeFile('ReadMe.md', data, err => {
+        if (err) {
+            console.log(err);
+            return;
+        } else {
+            console.log('Your ReadMe has been Created')
+        }
+    })
+}
 
-// Function call to initialize app
-init();
+// // TODO: Create a function to initialize app
+questions().then(answers => {
+    return generatePage(answers);
+}).then(data => {
+    return writeFile(data);
+}).catch(err => {
+    console.log(err)
+})
